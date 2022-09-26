de3be3ce930b738c5a18d4cfc98bf79d3dc04e82
Kevin <you@example.com>
2022-09-26 17:01:07 +0800

    refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -34,12 +34,23 @@ class NewVisitorTest(TestCase):
         
         table = self.browser.find_element(By.ID, 'id_list_table')
 
-        inputbox.send_keys('buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
         wait = WebDriverWait(table, 10)
         wait.until(expected_conditions.staleness_of(table))
 
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        wait = WebDriverWait(table, 10)
+        wait.until(expected_conditions.staleness_of(table))
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
