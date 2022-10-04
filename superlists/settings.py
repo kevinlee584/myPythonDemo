576f2ca4ba65dd5761c8b92559972655f33a9ac3
Kevin <you@example.com>
2022-10-04 15:23:33 +0800

    modify something

--- superlists/settings.py
+++ superlists/settings.py
@@ -16,6 +16,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+print("-------" + os.path.join(BASE_DIR, '..', 'database', 'db.sqlite3') + "-----")
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
