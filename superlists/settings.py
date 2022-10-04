ae8a5693a845c616a94b27c10d96e670c1c34cac
Kevin <you@example.com>
2022-10-04 13:00:50 +0800

    modify settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's#t9if@i4m6sffg3iu!_em@am93lzp5)r*x6p=h@lzq3%iwsi$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
